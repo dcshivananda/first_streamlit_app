1dc48a88d675810d4a539fb7102b2be8dd3e3122
Jyoshi <94054419+dcshivananda@users.noreply.github.com>
2023-04-20 19:25:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
-import pandas as pd
-my_fruits_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.dataframe('my_fruits_list')
+import pandas
+my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruits_list)
